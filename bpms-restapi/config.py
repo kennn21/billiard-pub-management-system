c0ee183c0c1d5a09e18ab6e19140b5db3cbacfa6
kennn21 <91940623+kennn21@users.noreply.github.com>
2023-04-03 10:42:04 +0700

    Continued restapi
    - Added CRUD for receipt

--- bpms-restapi/config.py
+++ bpms-restapi/config.py
@@ -3,9 +3,9 @@ from flaskext.mysql import MySQL
 
 mysql = MySQL()
 
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'flask_rest_api'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'sql12610273'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'A8UYGQgGGJ'
+app.config['MYSQL_DATABASE_DB'] = 'sql12610273'
+app.config['MYSQL_DATABASE_HOST'] = 'sql12.freesqldatabase.com'
 
 mysql.init_app(app)
--- bpms-restapi/main.py
+++ bpms-restapi/main.py
@@ -4,8 +4,10 @@ from config import mysql
 from flask import jsonify
 from flask import flash, request
 
+# TABLES
 
-@app.route('/create', methods=['POST'])
+
+@app.route('/create/table', methods=['POST'])
 def create_table():
     conn = mysql.connect()
     cursor = conn.cursor()
@@ -54,8 +56,8 @@ def tables():
         conn.close()
 
 
-@app.route('/tables/')
-def emp_details(table_id):
+@app.route('/tables/<table_id>')
+def table_details(table_id):
     try:
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
@@ -73,7 +75,7 @@ def emp_details(table_id):
         conn.close()
 
 
-@app.route('/update', methods=['PUT'])
+@app.route('/update/table/<table_id>', methods=['PUT'])
 def update_table():
     try:
         _json = request.json
@@ -102,8 +104,8 @@ def update_table():
         conn.close()
 
 
-@app.route('/delete/<table_id>', methods=['DELETE'])
-def delete_emp(table_id):
+@app.route('/delete/table/<table_id>', methods=['DELETE'])
+def delete_table(table_id):
     try:
         conn = mysql.connect()
         cursor = conn.cursor()
@@ -111,6 +113,127 @@ def delete_emp(table_id):
                        (table_id,))
         conn.commit()
         response = jsonify(f'Table {table_id} deleted successfully!')
+        response.status_code = 200
+        return response
+    except Exception as e:
+        print(e)
+        return jsonify(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+# Receipts
+
+
+@app.route('/create/receipt', methods=['POST'])
+def create_receipt():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    try:
+        _json = request.json
+        _receipt_id = _json['receipt_id']
+        _food_orders = _json['food_orders']
+        _total_price = _json['total_price']
+        _start_time = _json['start_time']
+        _end_time = _json['end_time']
+        if _receipt_id and _food_orders and _total_price and _start_time and _end_time and request.method == 'POST':
+            conn = mysql.connect()
+            cursor = conn.cursor(pymysql.cursors.DictCursor)
+            sqlQuery = '''INSERT INTO receipts(receipt_id, food_orders, total_price, start_time, end_time) VALUES (%s, %s, %s, %s, %s)'''
+            bindData = (_receipt_id, _food_orders,
+                        _total_price, _start_time, _end_time)
+            cursor.execute(sqlQuery, bindData)
+            conn.commit()
+            response = jsonify('Receipt added successfully')
+            response.status_code = 200
+            return response
+        else:
+            return showMessage()
+    except Exception as e:
+        print(e)
+        return jsonify('error')
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.route("/receipts")
+def receipts():
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(
+            "SELECT receipt_id, food_orders, total_price, start_time, end_time FROM receipts")
+        receiptsRows = cursor.fetchall()
+        response = jsonify(receiptsRows)
+        response.status_code = 200
+        return response
+    except Exception as e:
+        print(e)
+        return jsonify(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.route('/receipt/<receipt_id>')
+def receipt_details(receipt_id):
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(
+            "SELECT receipt_id, food_orders, total_price, start_time, end_time FROM tables WHERE id = %s", receipt_id)
+        receiptRow = cursor.fetchone()
+        response = jsonify(receiptRow)
+        response.status_code = 200
+        return response
+    except Exception as e:
+        print(e)
+        return jsonify(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.route('/update/receipt/<receipt_id>', methods=['PUT'])
+def update_receipt():
+    try:
+        _json = request.json
+        _receipt_id = _json['receipt_id']
+        _food_orders = _json['food_orders']
+        _total_price = _json['total_price']
+        _start_time = _json['start_time']
+        _end_time = _json['end_time']
+        if _receipt_id and _food_orders and _total_price and _start_time and _end_time and request.method == 'PUT':
+            sqlQuery = "UPDATE receipts SET receipt_id=%s, food_orders=%s, total_price=%s, start_time=%s, end_time=%s WHERE table_id=%s"
+            bindData = (_receipt_id, _food_orders, _total_price,
+                        _start_time, _end_time, _receipt_id)
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.execute(sqlQuery, bindData)
+            conn.commit()
+            response = jsonify(f'Receipt {_receipt_id} updated successfully!')
+            response.status_code = 200
+            return response
+        else:
+            return showMessage()
+    except Exception as e:
+        print(e)
+        return jsonify(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.route('/delete/receipt/<receipt_id>', methods=['DELETE'])
+def delete_receipt(receipt_id):
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM receipt WHERE table_id =%s",
+                       (receipt_id,))
+        conn.commit()
+        response = jsonify(f'Receipt {receipt_id} deleted successfully!')
         response.status_code = 200
         return response
     except Exception as e:
